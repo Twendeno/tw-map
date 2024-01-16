ccf007687ab06432280a0acc2619e6fe0e77c1bd
styve Lioumba <styve.lioumba@jdc.fr>
2024-01-16 18:20:00 +0100

    base du multilangue

--- src/app/modules/menu/menu-list/menu-list.component.ts
+++ src/app/modules/menu/menu-list/menu-list.component.ts
@@ -1,26 +1,51 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
 import {CommonModule, NgOptimizedImage} from '@angular/common';
 import {ItemMenuCardComponent} from "@app/components/item-menu-card/item-menu-card.component";
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {TranslateService} from "@ngx-translate/core";
+import {LanguageService} from "@app/services/language.service";
+import {TranslationModule} from "@app/modules/translation/translation.module";
 
 @Component({
   selector: 'app-menu-list',
   standalone: true,
-  imports: [CommonModule, ItemMenuCardComponent, RouterLink, RouterLinkActive, NgOptimizedImage, RouterOutlet],
+  imports: [CommonModule, ItemMenuCardComponent, RouterLink, RouterLinkActive, NgOptimizedImage, RouterOutlet, TranslationModule],
   templateUrl: './menu-list.component.html',
   styleUrl: './menu-list.component.css'
 })
-export class MenuListComponent {
+export class MenuListComponent implements OnInit , OnDestroy{
 
   menuList = [
-    {icon: 'assets/icons/itineraire.svg', title: 'Itinéraires', description: 'Je calcule mon itineraire en transport en commun.', route: '/itineraires'},
-    {icon: 'assets/icons/time.svg', title: 'Horaires des lignes', description: 'Je consulte les horaires de passage.', route: '/lines'},
-    {icon: 'assets/icons/search.svg', title: 'Rechercher', description: 'Je recherche un arrêt, une ligne, un lieu d\'intérêt ou une commune.', route: '/recherche'},
+    {icon: 'assets/icons/itineraire.svg', title: 'menuList.cardItem.title.one', description: 'menuList.cardItem.description.one', route: '/itineraires'},
+    {icon: 'assets/icons/time.svg', title: 'menuList.cardItem.title.two', description: 'menuList.cardItem.description.two', route: '/lines'},
+    {icon: 'assets/icons/search.svg', title: 'menuList.cardItem.title.three', description: "menuList.cardItem.description.three", route: '/recherche'},
   ];
 
   sousMenuList = [
-    {icon:'assets/icons/ticket.svg',title:'Points de vente',route:'/points-de-vente'},
-    {icon:'assets/icons/agences.svg',title:'Nos agences',route:'/agences'},
+    {icon:'assets/icons/ticket.svg',title:'menuList.title.one',route:'/points-de-vente'},
+    {icon:'assets/icons/agences.svg',title:'menuList.title.two',route:'/agences'},
   ];
+
+
+  readonly languageService$ = inject(LanguageService)
+  private readonly translate$= inject(TranslateService);
+
+  ngOnInit(): void {
+    this.languageService$.getLanguage().subscribe({
+      next:(newLanguage: string)=>{
+        this.translate$.use(newLanguage); // Langue initiale
+      },
+      complete:()=>{
+        this.languageService$.getLanguage().unsubscribe()
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.languageService$.getLanguage().unsubscribe()
+  }
+
+
+
 
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,13 +1,43 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {Component, inject, OnDestroy, OnInit} from '@angular/core';
+import { CommonModule, NgOptimizedImage } from '@angular/common';
+import {TranslateModule, TranslateService} from "@ngx-translate/core";
+import {TranslationModule} from "@app/modules/translation/translation.module";
+import {LanguageService} from "@app/services/language.service";
+import {TreeSelectModule} from "primeng/treeselect";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,NgOptimizedImage, TranslateModule, TranslationModule, TreeSelectModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit , OnDestroy{
+
+
+  private readonly translate$= inject(TranslateService);
+  readonly languageService$ = inject(LanguageService)
+
+
+  ngOnInit(): void {
+    this.languageService$.getLanguage().subscribe({
+      next:(newLanguage: string)=>{
+        this.translate$.setDefaultLang(newLanguage); // Langue par défaut
+        this.translate$.use(newLanguage); // Langue initiale
+      },
+      complete:()=>{
+        this.languageService$.getLanguage().unsubscribe()
+      }
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.languageService$.getLanguage().unsubscribe()
+  }
+
+  public selectLanguage(event: any) {
+    this.translate$.use(event.target.value);
+    this.languageService$.setLanguage(event.target.value)
+  }
 
 }
