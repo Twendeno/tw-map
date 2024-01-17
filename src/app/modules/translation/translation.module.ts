8b5813c0733eb726d833ee71b07d2550243542c8
Styve Lioumba <37368019+StyveLioumba@users.noreply.github.com>
2024-01-17 10:50:31 +0100

    Feat/internationalisation (#2)
    * configuration du premier workflow
    
    * release conf updated
    
    * release conf updated on git push --set-upstream
    
    * cartographie de pointe-noire
    
    * base du multilangue
    
    ---------
    
    Co-authored-by: styve Lioumba <styve.lioumba@jdc.fr>

---
+++ src/app/modules/translation/translation.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HttpClient} from "@angular/common/http";
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
+  ],
+  exports: [TranslateModule]
+})
+export class TranslationModule { }
